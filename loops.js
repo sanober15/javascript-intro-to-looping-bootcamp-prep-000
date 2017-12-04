e82c6858cabc2706f32282bfbfd52ed7c385d3c3
sanober15 <sanober@sanober.me>
2017-12-04 11:22:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
   for (var i = 0; i<25; i++){
     if (i === 1) {
-      array.shift("I am 1 strange loop.");
+      return array.shift["I am 1 strange loop."];
     }
     else {
-      array.shift("I am ${i} strange loops.");
+      return array.shift["I am ${i} strange loops."];
     }
   }
 }
