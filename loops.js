bbcce810a3256f4e98097ff339c9f71672e16844
sanober15 <sanober@sanober.me>
2017-12-04 11:21:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array){
   for (var i = 0; i<25; i++){
     if (i === 1) {
-      console.log(array + "I am 1 strange loop.");
+      array.push("I am 1 strange loop.");
     }
     else {
-      console.log(array + "I am ${i} strange loops.");
+      array.push("I am ${i} strange loops.");
     }
   }
 }
